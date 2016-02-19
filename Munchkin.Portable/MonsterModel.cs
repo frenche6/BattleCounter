52d187c99bf451361aee38e78c6b0eff313b6dc9
Thomas French <frenche6_2004@yahoo.com>
2016-02-19 09:35:50 -0500

    Added files via upload

---
+++ Munchkin.Portable/MonsterModel.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+
+
+namespace Munchkin.Portable.Core
+{
+    public class MonsterModel : INotifyPropertyChanged
+    {
+        private int mMonsterLevel = 1;
+        public int MonsterLevel
+        {
+            get
+            {
+                return mMonsterLevel;
+            }
+            set
+            {
+                mMonsterLevel = value;
+                CalculateTotal();
+                RaisePropertyChanged("MonsterLevel");
+            }
+        }
+
+        private int mTemporaryBonus = 0;
+        public int TemporaryBonus
+        {
+            get
+            {
+                return mTemporaryBonus;
+            }
+            set
+            {
+                mTemporaryBonus = value;
+                CalculateTotal();
+                RaisePropertyChanged("TemporaryBonus");
+            }
+        }
+
+        private int mTotalPower = 1;
+        public int TotalPower
+        {
+            get
+            {
+                return mTotalPower;
+            }
+            set
+            {
+                mTotalPower = value;
+                RaisePropertyChanged("TotalPower");
+            }
+        }
+
+        private void CalculateTotal()
+        {
+            TotalPower = mMonsterLevel + mTemporaryBonus;
+        }
+
+        public void ResetValues()
+        {
+            MonsterLevel = 1;
+            TemporaryBonus = 0;
+            TotalPower = 1;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
